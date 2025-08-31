2ddf595529e2f1a450b9d3253e803a64a30f87e4
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2025-09-01 02:06:35 +0600

    add: variables & data type folder

---
+++ Variables & Data Type/first.js
@@ -0,0 +1,24 @@
+// console.log("I love JavaScript");
+// let fullName="Tony Stark"
+// let age=24;
+// let price=99.99;
+// let redius=14;
+// let x=null;
+// let y=undefined;
+// let isFollow=true;
+// console.log(y)
+
+const product = {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+}
+
+const profile = {
+    userName: "shradhakhapra",
+    isFollow: "true",
+    post: 197,
+    followers: 569,
+    following: 4
+}
