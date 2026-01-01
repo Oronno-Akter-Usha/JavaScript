12c3d2a6507029949861baed4a8c1c8c99fa2bd6
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-01 15:36:29 +0600

    add: operators and conditional statment folder

---
+++ Operators and Conditional Statement/Note.js
@@ -0,0 +1,14 @@
+// -----Comments-----
+// Part of Code which is not executed
+//This is a single line comment
+/*This is a multi-line
+comment. */
+
+// -----Operators-----
+// Used to perform some opration on data
+// Arithmetic Operators
+// +, -, *, /
+// Modulus = %
+// Exponentiation = **
+// Increment = ++
+// Decrement = --
---
+++ Operators and Conditional Statement/script.js
@@ -0,0 +1,21 @@
+// Artimetic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, "& b =", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+// Unary Operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b =", b);
+
+console.log("++a =", ++a); //6 (++a= pre)
+console.log("a++ =", a++); //5(a++ = post)
+console.log("a = ", a); //6
