9dc4a82cdc0c2078c8627114e317313e3da8b1a4
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-07 22:04:40 +0600

    add: 2nd  practice question

--- Operators and Conditional Statement/Practice.js
+++ Operators and Conditional Statement/Practice.js
@@ -1,9 +1,32 @@
 // Question-1. Get user to input a number using prompt ("Enter a number":).check if the number is a multiple of 5 or not
 
-let num = prompt("Enter a number");
+// let num = prompt("Enter a number");
 
-if (num % 5 === 0) {
-  console.log(num, "is multiple of 5");
-} else {
-  console.log(num, "is not multiple of 5");
+// if (num % 5 === 0) {
+//   console.log(num, "is multiple of 5");
+// } else {
+//   console.log(num, "is not multiple of 5");
+// }
+
+// Question-2. Write a code which can give grades to students according to their scores:
+// 90-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+let score = 100;
+let grade;
+if (score >= 90 && score <= 100) {
+  grade = "A";
+} else if (score >= 70 && score <= 89) {
+  grade = "B";
+} else if (score >= 60 && score <= 69) {
+  grade = "C";
+} else if (score >= 50 && score <= 59) {
+  grade = "D";
+} else if (score >= 0 && score <= 49) {
+  grade = "F";
 }
+
+console.log("According to your scores, your grade was : ", grade);
