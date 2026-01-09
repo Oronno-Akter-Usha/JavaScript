35997746dff9d1e1c02853bac262e91827414386
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-09 20:40:23 +0600

    add: for loop

---
+++ Loops and Strings/script.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+
+for (let count = 1; count <= 5; count++) {
+  console.log("hello");
+}
