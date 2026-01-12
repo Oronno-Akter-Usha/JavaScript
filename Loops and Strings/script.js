7d1e5098f98fe9a2cc0423ad6cd31702c89b91e8
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-13 00:43:12 +0600

    add: while loop

--- Loops and Strings/script.js
+++ Loops and Strings/script.js
@@ -4,15 +4,23 @@
 // console.log("hello");
 // console.log("hello");
 
+// -----for loop-----
 // Print 1 to 5
-for (let i = 1; i <= 5; i++) {
-  console.log("i", i);
-}
+// for (let i = 1; i <= 5; i++) {
+//   console.log("i", i);
+// }
 
 // Calculate sum of 1 to n
-let sum = 0;
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  sum = sum + i;
+// let sum = 0;
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// -----while loop-----
+let i = 1;
+while (i <= 6) {
+  console.log(i);
+  i++;
 }
-console.log(sum);
