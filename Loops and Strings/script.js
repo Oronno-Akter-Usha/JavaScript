76bf74f15d06ee8fc05bbfdaad6d73c98337a722
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-12 20:17:36 +0600

    add: calculate sum of 1 to n

--- Loops and Strings/script.js
+++ Loops and Strings/script.js
@@ -4,6 +4,15 @@
 // console.log("hello");
 // console.log("hello");
 
-for (let count = 1; count <= 5; count++) {
-  console.log("hello");
+// Print 1 to 5
+for (let i = 1; i <= 5; i++) {
+  console.log("i", i);
 }
+
+// Calculate sum of 1 to n
+let sum = 0;
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+}
+console.log(sum);
