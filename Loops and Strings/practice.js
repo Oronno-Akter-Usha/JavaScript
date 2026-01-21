660999909ea42aa9666098a7cd2769abef435a23
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-21 22:31:22 +0600

    add: 2nd practice question

--- Loops and Strings/practice.js
+++ Loops and Strings/practice.js
@@ -1,7 +1,16 @@
 // Question-1. Print all even numbers from 0 to 100
 
-for (let n = 0; n <= 100; n++) {
-  if (n % 2 == 0) {
-    console.log(n);
-  }
+// for (let n = 0; n <= 100; n++) {
+//   if (n % 2 === 0) {
+//     console.log(n);
+//   }
+// }
+
+// Question- 2. Create a game where you start with any random game number. Ask the user to keep gussing the game number until the user enter correct value.
+
+let gameNum = 25;
+let userNum = prompt("Guess the game number : ");
+while (userNum != gameNum) {
+  userNum = prompt("You entered worng number. Guess again : ");
 }
+console.log("Congratulations, you entered the right number");
