591e241c80a63d634880ef1aac8564909c52aa00
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-24 23:37:16 +0600

    add: string methods

--- Loops and Strings/script.js
+++ Loops and Strings/script.js
@@ -81,5 +81,41 @@
 // let str = "Hello\nWorld";// \n = next line
 // console.log(str);
 
-let str = "Hello\tWorld"; // \t = tab space
-console.log(str.length);
+// let str = "Hello\tWorld"; // \t = tab space
+// console.log(str.length);
+
+// -----String Methods-----
+// ---toUpperCase---
+// let str = "HelloWorld";
+// let newstr = str.toUpperCase();
+// console.log(str);
+// console.log(newstr);
+
+// ---tolowerCase---
+// let str = "HelloWorld";
+// let newstr = str.toLowerCase();
+// console.log(str);
+// console.log(newstr);
+
+// ---trim--- [remove starting and end white space]
+// let str = "  Hello    World  ";
+// let newstr = str.trim();
+// console.log(str);
+// console.log(newstr);
+
+// ---slice---
+// let str = "01234567";
+// console.log(str.slice(1, 5));
+
+// ---concat---
+// let str1 = "Hello";
+// let str2 = "World";
+// console.log(str1.concat(str2));
+
+// ---replace---
+// let str = "hello";
+// console.log(str.replace("lo", "p"));
+
+// ---charAt---
+let str = "IloveJS";
+console.log(str.charAt(6));
