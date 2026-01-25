138750086f6cf59a513fce1511ad58ba0f068645
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-25 23:45:25 +0600

    add: practice question

--- Loops and Strings/practice.js
+++ Loops and Strings/practice.js
@@ -14,3 +14,10 @@
 //   userNum = prompt("You entered worng number. Guess again : ");
 // }
 // console.log("Congratulations, you entered the right number");
+
+// Question-3. Prompt the user to enter their full name. Generate a username for them based on the input.Start username with @, followed by their full name and ending with the fullname length.
+
+let fullName = prompt("enter your fullname without spaces");
+
+let username = "@" + fullName + fullName.length;
+console.log(username);
