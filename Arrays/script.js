a3a56d8e4e64d75b7784f21f1c8152f4d86b53b9
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-01-29 23:57:57 +0600

    looping over arrays

--- Arrays/script.js
+++ Arrays/script.js
@@ -7,7 +7,19 @@
 // console.log(heroes);
 
 // -----Indices----
-let marks = [90, 78, 57, 65, 40];
-marks[0] = 99;
-console.log(marks);
-console.log(marks[0]);
+// let marks = [90, 78, 57, 65, 40];
+// marks[0] = 99;
+// console.log(marks);
+// console.log(marks[0]);
+
+// -----looping over arrays----
+let heroes = ["ironman", "thor", "hulk", "spiderman"];
+// for loop
+for (let i = 0; i < heroes.length; i++) {
+  console.log(heroes[i]);
+}
+
+// for of
+for (let hero of heroes) {
+  console.log(hero);
+}
