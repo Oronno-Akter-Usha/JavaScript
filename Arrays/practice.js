3c80b4473c1cfbeeddbc65f4d45392c3b701dfa8
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-02-01 23:46:57 +0600

    add: practice 2nd practice question

--- Arrays/practice.js
+++ Arrays/practice.js
@@ -1,11 +1,31 @@
 // Question-1. For a given array with marks of students ->[80,97,42,53]
 // Find the average marks of the entries class.
 
-let marks = [80, 97, 42, 53, 86, 68];
-let sum = 0;
-for (let val of marks) {
-  sum += val;
+// let marks = [80, 97, 42, 53, 86, 68];
+// let sum = 0;
+// for (let val of marks) {
+//   sum += val;
+// }
+
+// let avg = sum / marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+// Question-2. For a given array with prices of 5 items -> [250, 645, 300, 900, 50]
+// All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
+
+let items = [250, 645, 300, 900, 50];
+
+// let i = 0;
+// for (let val of items) {
+//   let offer = val / 10;
+//   items[i] = items[i] - offer;
+//   console.log(`value after offer =${items[i]}`);
+//   i++;
+// }
+
+for (let i = 0; i < items.length; i++) {
+  let offer = items[i] / 10;
+  items[i] -= offer;
 }
 
-let avg = sum / marks.length;
-console.log(`avg marks of the class = ${avg}`);
+console.log(items);
