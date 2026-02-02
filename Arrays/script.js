0345925382c250406b1adf7a7e92270ccdf1fde0
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-02-02 23:55:49 +0600

    add: push method

--- Arrays/script.js
+++ Arrays/script.js
@@ -13,13 +13,20 @@
 // console.log(marks[0]);
 
 // -----looping over arrays----
-let heroes = ["ironman", "thor", "hulk", "spiderman"];
+// let heroes = ["ironman", "thor", "hulk", "spiderman"];
 // for loop
-for (let i = 0; i < heroes.length; i++) {
-  console.log(heroes[i]);
-}
+// for (let i = 0; i < heroes.length; i++) {
+//   console.log(heroes[i]);
+// }
 
 // for of
-for (let hero of heroes) {
-  console.log(hero);
-}
+// for (let hero of heroes) {
+//   console.log(hero);
+// }
+
+// -----Arrays Methods-----
+// ---push() : add to end---
+let foodItems = ["apple", "potato", "litchi", "tomato"];
+console.log(foodItems);
+foodItems.push("chips", "burger");
+console.log(foodItems);
