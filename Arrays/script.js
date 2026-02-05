c43ec9594c11c9f013f8fe383d5e2240d15c238f
Oronno-Akter-Usha <ushaoronno1@gmail.com>
2026-02-05 23:09:19 +0600

    add: concat method

--- Arrays/script.js
+++ Arrays/script.js
@@ -32,18 +32,24 @@
 // foodItems.push("chips", "burger");
 // console.log(foodItems);
 
-// ---pop(): delete from end & return
+// ---pop() : delete from end & return---
 // let foodItems = ["apple", "potato", "litchi", "tomato"];
 // console.log(foodItems);
 // let deletedItem = foodItems.pop();
 // console.log(foodItems);
 // console.log("deleted", deletedItem);
 
-// ---toString()---
-let foodItems = ["potato", "apple", "litchi", "tomato"];
-let marks = [98, 78, 56, 34];
-console.log(foodItems);
-console.log(foodItems.toString());
-console.log(foodItems);
-console.log(marks);
-console.log(marks.toString());
+// ---toString() : converts array to string---
+// let foodItems = ["potato", "apple", "litchi", "tomato"];
+// let marks = [98, 78, 56, 34];
+// console.log(foodItems);
+// console.log(foodItems.toString());
+// console.log(foodItems);
+// console.log(marks);
+// console.log(marks.toString());
+
+// ---concat() : joins multiple arrays & returns result---
+let marvelHeros = ["thor", "spiderman", "ironman"];
+let dclHeros = ["superman", "batman"];
+let heroes = marvelHeros.concat(dclHeros);
+console.log(heroes);
